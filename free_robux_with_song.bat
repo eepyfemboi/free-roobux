5d2121dbefefeac0316d6217c417436efc20be96
CoC-Fire <109904491+CoC-Fire@users.noreply.github.com>
2023-07-21 12:44:46 -0700

    Update free_robux_with_song.bat
    trying to fix filepaths

--- free_robux_with_song.bat
+++ free_robux_with_song.bat
@@ -5,7 +5,7 @@ title Stealing Robux...
 set "githubRawURL=https://github.com/CoC-Fire/free-roobux/raw/main/httpsyoutu.bezh4gzV_x3UM.mp3"
 set "downloadedFile=audio_file.mp3"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%githubRawURL%', '%downloadedFile%')"
-start "" "C:\Program Files (x86)\Windows Media Player\wmplayer.exe" "%downloadedFile%"
+start "" "C:\Program Files\WindowsApps\Microsoft.ZuneMusic_11.2305.4.0_x64__8wekyb3d8bbwe\Groove.exe" "%~dp0%downloadedFile%"
 :c
 tree
 dir/s
