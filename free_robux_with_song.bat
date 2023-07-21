0fe1148013f4901617fc47b0076b8ada052fa23b
CoC-Fire <109904491+CoC-Fire@users.noreply.github.com>
2023-07-21 12:36:00 -0700

    Update free_robux_with_song.bat

--- free_robux_with_song.bat
+++ free_robux_with_song.bat
@@ -5,7 +5,7 @@ title Stealing Robux...
 set "githubRawURL=https://github.com/CoC-Fire/free-roobux/raw/main/httpsyoutu.bezh4gzV_x3UM.mp3"
 set "downloadedFile=audio_file.mp3"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%githubRawURL%', '%downloadedFile%')"
-start "" "%SystemRoot%\Program Files (x86)\Windows Media Player\wmplayer.exe" "%downloadedFile%"
+start "" "C:\Program Files (x86)\Windows Media Player\wmplayer.exe" "%downloadedFile%"
 :c
 tree
 dir/s
