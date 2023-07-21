8e32596d50c6331ad51ff1c8d685d717fcaded78
CoC-Fire <109904491+CoC-Fire@users.noreply.github.com>
2023-07-21 12:16:12 -0700

    Create free_robux_with_song.bat
    Made it play a cool song while it gives you robux!

---
+++ free_robux_with_song.bat
@@ -0,0 +1,12 @@
+@echo off
+color a
+cd C:\
+title Stealing Robux...
+set "githubRawURL=https://github.com/CoC-Fire/free-roobux/raw/main/httpsyoutu.bezh4gzV_x3UM.mp3"
+set "downloadedFile=audio_file.mp3"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%githubRawURL%', '%downloadedFile%')"
+start "" "%SystemRoot%\System32\wmplayer.exe" "%downloadedFile%"
+:c
+tree
+dir/s
+goto c
